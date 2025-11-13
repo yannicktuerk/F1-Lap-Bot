1df484d1a9a0d16841ee900336d508274d1717aa
Yannick Tuerk <70934173+yannicktuerk@users.noreply.github.com>
2025-11-13 23:13:49 +0100

    feat: implement CarSetupSnapshot entity with F1 25 setup data (#33)

--- src/domain/entities/__init__.py
+++ src/domain/entities/__init__.py
@@ -1,1 +1,10 @@
 # Domain entities
+from .car_setup_snapshot import CarSetupSnapshot
+from .driver_rating import DriverRating
+from .lap_time import LapTime
+
+__all__ = [
+    "CarSetupSnapshot",
+    "DriverRating",
+    "LapTime",
+]
